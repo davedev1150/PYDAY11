86ba6472254fcf0eb0ef901860ac16755bd63ddc
davedev1150 <102778924+davedev1150@users.noreply.github.com>
2023-08-09 20:40:45 +0700

    Add GradeCal function

--- program.py
+++ program.py
@@ -7,3 +7,16 @@ print("1234")
 print("bye bye")
 print("test test")
 
+def grade_calculate(score):
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
+    
+print(grade_calculate(85))
